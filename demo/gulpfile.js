91fa99c731268d41cc6df7f206ef2e9cc69ba6cd
Romuald Brillout <github.com@brillout.com>
2015-09-23 20:04:37 +0200

    add usage examples to demo gulpfile

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -1,8 +1,40 @@
 var gulp = require('gulp');
 var gulp_jspm = require('../index.js');
 
+
 gulp.task('default', function(){
     return gulp.src('src/main.js')
         .pipe(gulp_jspm())
         .pipe(gulp.dest('build/'));
+});
+
+
+gulp.task('sourcemap', function(){
+    var sourcemaps = require('gulp-sourcemaps');
+
+    return gulp.src('src/main.js')
+        .pipe(sourcemaps.init())
+        .pipe(gulp_jspm())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/'));
+});
+
+
+gulp.task('test', function(){
+    var assert = require('better-assert');
+
+    return gulp.src('src/main.js')
+        .pipe(gulp_jspm())
+        .pipe(pass(function(vinyl_file){
+            assert( vinyl_file.relative === 'jspm-bundle.js' );
+            assert( vinyl_file.original_entry_point.relative === 'main.js' );
+        }));
+
+    function pass(fct) {
+        return require('through2').obj(function(file, enc, cb){
+            fct(file);
+            this.push(file);
+            cb();
+        });
+    }
 });
