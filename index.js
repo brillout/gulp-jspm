8cd6e6fb869d3f084975bd9564113748d815afd7
Romuald Brillout <github.com@brillout.com>
2016-02-14 23:04:38 +0100

    improve code readability -- always pass same object holding infos throughout promises + separate stream logic from jspm bundling logic

--- index.js
+++ index.js
@@ -8,142 +8,172 @@ var temp = require('temp').track();
 var File = require('vinyl');
 var fs = Promise.promisifyAll(require("fs"));
 var path = require('path');
-
-
 var projectName = require('./package.json').name;
 
+
 module.exports = function(opts){
-    opts = opts || {};
 
     return through.obj(function(file, enc, cb){
+
         if( file.isNull() ){
             cb();
             return;
         }
+
         if( file.isStream() ){
             this.emit('error', new gutil.PluginError(projectName, 'Streams are not supported.'));
             cb();
             return;
         }
 
-        var enable_source_map = !!file.sourceMap;
-
         var push = this.push.bind(this);
-
-        Promise.resolve()
-        .then(function(){
-            return set_jspm_package_path(file.base);
-        })
-        .then(function(jspmRoot){
-            return [Promise.promisify(temp.open)('gulp-jspm__build.js'), Promise.resolve(jspmRoot)];
-        })
-        .spread(function(tmp_file, jspmRoot){
-            return (
-                jspm[opts.selfExecutingBundle?'bundleSFX':'bundle'](
-                    (function(){
-                        var jspm_input = path.relative(jspmRoot, file.path);
-                        if( opts.plugin ) {
-                            jspm_input += '!';
-                            if( opts.plugin.constructor === String ) {
-                                jspm_input += opts.plugin;
-                            }
-                        }
-                        if( opts.arithmetic ) {
-                            jspm_input += ' ' + opts.arithmetic.trim();
-                        }
-                        return jspm_input;
-                    })() ,
-                    tmp_file.path ,
-                    (function(){
-                        var jspm_opts = {};
-                        for(var i in opts) jspm_opts[i] = opts[i];
-                        jspm_opts.sourceMaps = jspm_opts.sourceMaps || enable_source_map;
-                        delete jspm_opts.plugin;
-                        delete jspm_opts.arithmetic;
-                        delete jspm_opts.selfExecutingBundle;
-                        return jspm_opts;
-                    })()
-                )
-                .then(function(){
-                    return tmp_file.path;
-                })
-            );
-        })
-        .then(function(temp_path){
-            var results = {
-                temp_path: temp_path
-            };
-            return Promise.all(
-                [
-                    fs.readFileAsync(temp_path)
-                    .then(function(file_content){
-                        if( enable_source_map ) {
-                            var reSourceMapComment = /\n\/\/# sourceMappingURL=.+?$/;
-                            results.contents = new Buffer(file_content.toString().replace(reSourceMapComment,''));
-                        }
-                        else {
-                            results.contents = file_content;
-                        }
-                    })
-                ].concat(
-                    ! enable_source_map ? [] : (
-                        fs.readFileAsync(temp_path+'.map')
-                        .then(function(file_content){
-                            results.sourceMap = JSON.parse(file_content.toString());
-                        })
-                    )
-                )
-            )
-            .then(function(){
-                return results;
-            });
-        })
-        .then(function(results){
-            temp.cleanup();
-            return results;
-        })
-        .then(function(results){
-            var bundle_file =
-                new File({
-                    base: file.base ,
-                    path: (function(){
-                        var basename = path.basename(file.path);
-                        basename = basename.split('.');
-                        basename.splice(1, 0, 'bundle');
-                        basename = basename.join('.');
-                        return path.join(path.dirname(file.path), basename);
-                    })() ,
-                    contents: results.contents
-                });
-
-            bundle_file.originalEntryPoint = file;
-
-            if( enable_source_map ) {
-                bundle_file.sourceMap = results.sourceMap;
-                bundle_file.sourceMap.file = bundle_file.relative;
-                bundle_file.sourceMap.sources =
-                    bundle_file.sourceMap.sources.map(function(relative_to_temp){
-                        return (
-                            path.relative(
-                                file.base,
-                                path.resolve(
-                                    path.dirname(results.temp_path),
-                                    relative_to_temp))
-                        );
-                    });
-            }
-
-            return bundle_file;
-        })
-        .then(function(bundle_file){
-            // timeout to stop Promise to catch errors
+        opts = opts || {};
+        do_bundle(file, opts)
+        .then(function(infos){
+            // 0ms Timeout in order to stop Promise to catch errors
             setTimeout(function(){
-                push(bundle_file);
+                push(infos.bundle.vinyl_file);
                 cb();
             },0);
         });
+
     });
+
 };
+
+function do_bundle(file, opts){
+
+    return Promise.resolve(
+        {
+            jspm: {
+                root: null,
+            },
+            bundle: {
+                path: null,
+                contents: null,
+                sourceMap: null,
+                vinyl_file: null
+            }
+        }
+    )
+    .then(function(infos){
+        return (
+            set_jspm_package_path(file.base)
+        ).then(function(jspm_root){
+            infos.jspm.root = jspm_root;
+            return infos;
+        });
+    })
+    .then(function(infos){
+        return (
+            Promise.promisify(temp.open)('gulp-jspm__build.js')
+        )
+        .then(function(temp_file){
+            infos.bundle.path = temp_file.path;
+            return infos;
+        });
+    })
+    .then(function(infos){
+        return (
+            jspm[opts.selfExecutingBundle?'bundleSFX':'bundle'](
+
+                // input
+                (function(){
+                    var jspm_input = path.relative(infos.jspm.root, file.path);
+                    if( opts.plugin ) {
+                        jspm_input += '!';
+                        if( opts.plugin.constructor === String ) {
+                            jspm_input += opts.plugin;
+                        }
+                    }
+                    if( opts.arithmetic ) {
+                        jspm_input += ' ' + opts.arithmetic.trim();
+                    }
+                    return jspm_input;
+                })() ,
+
+                // output
+                infos.bundle.path ,
+
+                // options
+                (function(){
+                    var jspm_opts = {};
+                    for(var i in opts) jspm_opts[i] = opts[i];
+                    jspm_opts.sourceMaps = jspm_opts.sourceMaps || file.sourceMap;
+                    delete jspm_opts.plugin;
+                    delete jspm_opts.arithmetic;
+                    delete jspm_opts.selfExecutingBundle;
+                    return jspm_opts;
+                })()
+            )
+            .then(function(){
+                return infos;
+            })
+        );
+    })
+    .then(function(infos){
+        return Promise.all(
+            [
+                fs.readFileAsync(infos.bundle.path)
+                .then(function(file_content){
+                    if( file.sourceMap ) {
+                        var reSourceMapComment = /\n\/\/# sourceMappingURL=.+?$/;
+                        infos.bundle.contents = new Buffer(file_content.toString().replace(reSourceMapComment,''));
+                    }
+                    else {
+                        infos.bundle.contents = file_content;
+                    }
+                })
+            ].concat(
+                ! file.sourceMap ? [] : (
+                    fs.readFileAsync(infos.bundle.path+'.map')
+                    .then(function(file_content){
+                        infos.bundle.sourceMap = JSON.parse(file_content.toString());
+                    })
+                )
+            )
+        )
+        .then(function(){
+            temp.cleanup();
+        })
+        .then(function(){
+            return infos;
+        });
+    })
+    .then(function(infos){
+        var bundle_file = infos.bundle.vinyl_file =
+            new File({
+                base: file.base ,
+                path: (function(){
+                    var basename = path.basename(file.path);
+                    basename = basename.split('.');
+                    basename.splice(1, 0, 'bundle');
+                    basename = basename.join('.');
+                    return path.join(path.dirname(file.path), basename);
+                })() ,
+                contents: infos.bundle.contents
+            });
+
+        bundle_file.originalEntryPoint = file;
+
+        if( file.sourceMap ) {
+            bundle_file.sourceMap = infos.bundle.sourceMap;
+            bundle_file.sourceMap.file = bundle_file.relative;
+            bundle_file.sourceMap.sources =
+                bundle_file.sourceMap.sources.map(function(relative_to_temp){
+                    return (
+                        path.relative(
+                            file.base,
+                            path.resolve(
+                                path.dirname(infos.bundle.path),
+                                relative_to_temp))
+                    );
+                });
+        }
+
+        return infos;
+    });
+}
 
 function set_jspm_package_path(directory){
     return new Promise(function(resolve){
